56c897653db3354dc7c9696a5fd896935eefb8a5
Andrija Eremic <andrija.eremic@gmail.com>
2023-12-04 21:42:28 +0100

    swagger

--- Gateway.CookUp/Program.cs
+++ Gateway.CookUp/Program.cs
@@ -3,16 +3,31 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Ocelot Configuration
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+// Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
-app.UseOcelot();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();
 
 app.Run();
