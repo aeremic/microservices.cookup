18a84852072bab57bea5b79184425021ba0834fe
Andrija Eremic <andrija.eremic@gmail.com>
2023-12-17 14:18:56 +0100

    added thumbnail path column

--- Recipes.Microservice/Domains/Recipe.cs
+++ Recipes.Microservice/Domains/Recipe.cs
@@ -6,6 +6,7 @@ public class Recipe
     public string? Name { get; set; }
     public string? Description { get; set; }
     public string? Instructions { get; set; }
+    public string? ThumbnailPath { get; set; }
     public DateTime? CreatedOn { get; set; }
     public long? CreatedBy { get; set; }
     public bool IsUserCreated { get; set; }
--- Recipes.Microservice/Migrations/20231217130524_added_thumbnail_path_column.Designer.cs
+++ Recipes.Microservice/Migrations/20231217130524_added_thumbnail_path_column.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Recipes.Microservice.Infrastructure;
@@ -11,9 +12,11 @@ using Recipes.Microservice.Infrastructure;
 namespace Recipes.Microservice.Migrations
 {
     [DbContext(typeof(Repository))]
-    partial class RepositoryModelSnapshot : ModelSnapshot
+    [Migration("20231217130524_added_thumbnail_path_column")]
+    partial class added_thumbnail_path_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
