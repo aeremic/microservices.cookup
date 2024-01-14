8008f6448aa0face3bdc248c75326b46676d97c6
Andrija Eremic <andrija.eremic@gmail.com>
2024-01-14 18:30:06 +0100

    fixed userdatadto model

--- Users.Microservice/Common/Models/DTOs/UserDataDto.cs
+++ Users.Microservice/Common/Models/DTOs/UserDataDto.cs
@@ -3,9 +3,10 @@
 public class UserDataDto
 {
     public long Id { get; set; }
-
-    public string? Email { get; set; }
-    public string? Firstname { get; set; }
-    public string? Lastname { get; set; }
-    public string? Image { get; set; }
+    
+    public Guid Guid { get; set; }
+    public required string Email { get; set; }
+    public required int Role { get; set; }
+    public string? Username { get; set; }
+    public string? ImageFullPath { get; set; }
 }
